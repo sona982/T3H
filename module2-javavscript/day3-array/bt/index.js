7448af23f9d52876f0d4e774552a77bd69fbbba5
sona982 <nguyentuanphong982@gmail.com>
2025-04-16 13:42:38 +0700

    update

--- module2-javavscript/day3-array/bt/index.js
+++ module2-javavscript/day3-array/bt/index.js
@@ -63,10 +63,10 @@
 // }
 
 // bai 6
-let arr = [1,2,3,4,5];
-while(arr.length > 1){
-    arr.pop();
-}
-console.log(`mang sau cung duoc chon con ${arr}`);
+// let arr = [1,2,3,4,5];
+// while(arr.length > 1){
+//     arr.pop();
+// }
+// console.log(`mang sau cung duoc chon con ${arr}`);
 
 // bai 7 
